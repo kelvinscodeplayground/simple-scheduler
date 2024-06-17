2d7bd11ad18fd2359096beea27f40e1e2a91c476
Kelvin Chin <chinkaiwen_35@outlook.com>
2024-06-18 02:04:05 +0800

    Added document formatter

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,24 +7,24 @@
 int main(int argc, char **argv)
 {
     using namespace std::chrono_literals;
-    bool isRunning{true};
+    bool isRunning { true };
     auto &schedule = Scheduler::getInstance();
 
-    schedule.schedule(5s, []()
-                      { std::cout << "Hello World" << std::endl; });
-    schedule.schedule(10s, []()
-                      { std::cout << "This is after 10s" << std::endl; });
-    schedule.schedule(15s, [&]()
-                      { std::cout << "Cleaning up" << std::endl; isRunning = false; });
+    schedule.schedule(5s, []() { std::cout << "Hello World" << std::endl; });
+    schedule.schedule(10s, []() { std::cout << "This is after 10s" << std::endl; });
+    schedule.schedule(15s, [&]() {
+        std::cout << "Cleaning up" << std::endl;
+        isRunning = false;
+    });
 
-    while (isRunning)
-    {
+    while (isRunning) {
 
         auto frameStart = std::chrono::steady_clock::now();
         Scheduler::getInstance().tick();
 
         auto frameEnd = std::chrono::steady_clock::now();
-        auto frameTime = std::chrono::duration_cast<std::chrono::milliseconds>(frameEnd - frameStart);
+        auto frameTime =
+                std::chrono::duration_cast<std::chrono::milliseconds>(frameEnd - frameStart);
         auto sleepTime = (1s / 60) - frameTime;
         std::this_thread::sleep_for(frameTime);
     }
